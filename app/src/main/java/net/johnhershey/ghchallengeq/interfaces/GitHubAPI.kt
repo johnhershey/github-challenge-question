2ff02a83399d45ebc23f59b5e15370e061e0cd1f
John <jhersheydev@outlook.com>
2021-03-06 11:27:15 -0600

    Started API.

---
+++ app/src/main/java/net/johnhershey/ghchallengeq/interfaces/GitHubAPI.kt
@@ -0,0 +1,11 @@
+package net.johnhershey.ghchallengeq.interfaces
+
+import net.johnhershey.ghchallengeq.models.ResponseItem
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface GitHubAPI {
+    @GET("brianchandotcom/liferay-portal/commits?per_page=3")
+
+    fun getCommitList(): Call<List<ResponseItem>>
+}
