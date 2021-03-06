408f6a316a0922a60e58c49a2f4357e151e0d4f2
John <jhersheydev@outlook.com>
2021-03-06 17:01:11 -0600

    Started Adapter.

--- app/src/main/java/net/johnhershey/ghchallengeq/MainActivity.kt
+++ app/src/main/java/net/johnhershey/ghchallengeq/MainActivity.kt
@@ -2,11 +2,27 @@ package net.johnhershey.ghchallengeq
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val BASE_URL = "https://api.github.com/repos/"
 
 class MainActivity : AppCompatActivity() {
+
+    var linearLayoutManager: LinearLayoutManager
+    var the Adapter: CommitsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerview_commits.layoutManager = linearLayoutManager
+
+        getCommitList()
+    }
+
+    private fun getCommitList() {
+
     }
 }
---
+++ app/src/main/java/net/johnhershey/ghchallengeq/adapters/CommitsAdapter.kt
@@ -0,0 +1,20 @@
+package net.johnhershey.ghchallengeq.adapters
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import net.johnhershey.ghchallengeq.models.ResponseItem
+
+class CommitsAdapter(val context: Context, val commitList: List<ResponseItem>): RecyclerView.Adapter<CommitsAdapter> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommitsAdapter {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CommitsAdapter, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
